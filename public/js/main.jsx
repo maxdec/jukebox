9e64cadba9a89462db53afe89c7ee973ee4c8007
Maxence <maxence@spotistic.com>
2015-02-23 19:28:06 +0100

    Fixed: error "socket connection interrupted" when closing the tab

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -2,6 +2,9 @@
 /* global io */
 
 window.socket = io.connect();
+$(window).on('beforeunload', function(){
+  window.socket.close();
+});
 
 var React = require('react/addons');
 var App = require('./components/App.jsx');
